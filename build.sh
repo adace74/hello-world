ad83a507f466510207f6184cc41604c155542894
Adam W. Dace <colonelforbin74@gmail.com>
2017-05-17 21:18:29 -0500

    Adding build framework.

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/usr/local/bin/bash
+######################################################################
+#
+# File: build.sh
+#
+# Copyright (c) 2017, Adam W. Dace.  All Rights Reserved.
+#
+# https://opensource.org/licenses/GPL-3.0
+#
+######################################################################
+ant $*
---
+++ src/com/github/console/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * $Id: dbc3d2575bbb47b0b23ca462acbb690f16d6dc47 $
+ *
+ * (c) Copyright 2013 Adam W. Dace.  All Rights Reserved.
+ * Please see the accompanying COPYING file for license information.
+ */
+
+package com.github.console;
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World!");
+    }
+}
